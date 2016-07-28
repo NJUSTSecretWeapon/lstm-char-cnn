7cd288769aaff7768c3dd436b087d3e43bf7fc49
Xiang Li (MSR Student-Person Consulting) <FAREAST.v-xialg@GCRGDL018.redmond.corp.microsoft.com>
2016-07-27 18:44:56 -0700

    add inneres

---
+++ log_layer1_res2.lua
@@ -0,0 +1,5 @@
+/home/v-xialg/torch/install/bin/luajit: main_inneres_lstm.lua:388: '=' expected near '+'
+stack traceback:
+	[C]: in function 'dofile'
+	...ialg/torch/install/lib/luarocks/rocks/trepl/scm-1/bin/th:145: in main chunk
+	[C]: at 0x00406670
--- model/ResMLP.lua
+++ model/ResMLP.lua
@@ -1,16 +1,35 @@
-local HighwayMLP = {}
+local ResMLP = {}
 
-function HighwayMLP.mlp(size, num_layers, bias, f)
+function ResMLP.mlp(size, num_layers, bias, f)
     -- size = dimensionality of inputs
     -- num_layers = number of hidden layers (default = 1)
     -- bias = bias for transform gate (default = -2)
     -- f = non-linearity (default = ReLU)
     
-    local output, transform_gate, carry_gate
+    local output, transform_gate, carry_gate, nx
     local num_layers = num_layers or 1
     local bias = bias or -2
     local f = f or nn.ReLU()
     local input = nn.Identity()()
+    
+    local inputs = {[1] = input}
+    if num_layers <= 0 then num_layers = 1 print('num_layers for resmlp in lstm must > 0') end
+    for i = 1, num_layers do
+        if i == 1 then
+            output = f(nn.Linear(size, 4 * size)(inputs[i]))
+            --nx     = nn.JoinTable(2)({inputs[i], inputs[i], inputs[i], inputs[i]})
+            nx     = nn.Reshape(4 * size, true)(nn.Replicate(4, 2)(inputs[i]))
+        else
+            output = f(nn.Linear(4 * size, 4 * size)(inputs[i]))
+            nx     = inputs[i]
+        end
+        output = nn.CAddTable()({output, nx})
+        table.insert(inputs, output)
+    end
+    return nn.gModule({input}, {output})
+end
+
+--[[reference down --
     local inputs = {[1]=input}
     for i = 1, num_layers do        
         output = f(nn.Linear(size, size)(inputs[i]))
@@ -22,6 +41,6 @@ function HighwayMLP.mlp(size, num_layers, bias, f)
         table.insert(inputs, output)
     end
     return nn.gModule({input},{output})
-end
+end]]
 
-return HighwayMLP
+return ResMLP
